38adb1aaeb78313236fd75e602ebe90cf8bea416
jihtin <129176693+jihtin@users.noreply.github.com>
2025-08-05 20:57:50 +0530

    feat: navbar

--- src/layouts/navbar/Navbar.jsx
+++ src/layouts/navbar/Navbar.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
-import logo from './assets/logo.png';
+import React, { useState } from 'react';
+import logo from './assets/logo1.png';
 
 const Navbar = () => {
+const [acive , setActive ] = useState('Home')
+const navbarItems = ['Home' , 'About' , 'Contact' , 'Login']
+
   return (
     <div className='flex justify-center mt-5 '>
       <nav className='w-[700px] h-[70px] bg-[#EFEFEF] rounded-full flex items-center justify-between px-8'>
         <img src={logo} alt="logo" className='w-32 h-auto' />
         <ul className='flex gap-8 font-semibold'>
-          <li className='cursor-pointer hover:text-gray-600'>About Us</li>
-          <li className='cursor-pointer hover:text-gray-600'>Contact Us</li>
-          <li className='cursor-pointer hover:text-gray-600'>Login</li>
+          {
+            navbarItems.map((item)=>(
+              <li
+              key={item}
+              className='cursor-pointer relative group px-2 py-1 '
+              onClick={()=> setActive(item)}
+              >{item}
+              <span className={`absolute bottom-0 left-0 h-[2px] bg-[#0a8ecf] transition-all duration-500 
+              ${acive === item ? 'w-full' : 'w-0 group-hover:w-full' }
+              `} 
+              
+              ></span>
+              </li>
+            ))
+          }
+        
         </ul>
       </nav>
     </div>
